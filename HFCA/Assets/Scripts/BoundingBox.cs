58c0fdac71176f08caafa9ff8e7430037e40e3c5
wanglin <wanglin@digi-spine.com>
2023-05-11 16:05:37 +0800

    Stash:stash the code and using Visual Effect Graph

--- HFCA/Assets/Scripts/BoundingBox.cs
+++ HFCA/Assets/Scripts/BoundingBox.cs
@@ -1,39 +1,55 @@
 using System;
 using System.Collections.Generic;
+using System.Xml.Schema;
+using JetBrains.Annotations;
 using Unity.VisualScripting.FullSerializer;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = System.Random;
 
 public class BoundingBox : MonoBehaviour
 {
-    private Bounds bounds;
-    private List<GameObject> boxList = new List<GameObject>();
-    
-    [SerializeField] private int div_x;
-    [SerializeField] private int div_y;
-    [SerializeField] private int div_z;
+    private Bounds _bounds;
+
+    //private readonly List<GameObject> _boxList = new List<GameObject>();
+    private List<Bounds> _boundsList = new List<Bounds>();
+
+    [SerializeField] private int divX;
+    [SerializeField] private int divY;
+    [SerializeField] private int divZ;
 
     private void Start()
     {
         var mesh = GetComponent<MeshFilter>().sharedMesh;
-        bounds = new Bounds(mesh.bounds.center, mesh.bounds.size);
-        
-        var size = bounds.size;
-        var minPos = bounds.min;
-        var stepX = size.x / div_x;
-        var stepY = size.y / div_y;
-        var stepZ = size.z / div_z;
+        _bounds = new Bounds(mesh.bounds.center, mesh.bounds.size);
 
-        for (var i = 0; i < div_x; i++)
+        // var size = _bounds.size;
+        // var minPos = _bounds.min;
+        //
+        // var stepX = size.x / divX;
+        // var stepY = size.y / divY;
+        // var stepZ = size.z / divZ;
+        
+
+        var divSize = new Vector3(_bounds.size.x / divX, _bounds.size.y / divY, _bounds.size.z / divZ);
+        var min = _bounds.min;
+
+        for (var x = 0; x < divX; x++)
         {
-            for (var j = 0; j < div_y; j++)
+            for (var y = 0; y < divY; y++)
             {
-                for (var k = 0; k < div_z; k++)
+                for (var z = 0; z < divZ; z++)
                 {
-                    var pos = new Vector3(minPos.x + stepX * i, minPos.y + stepY * j, minPos.z + stepZ * k);
-                    var box = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    box.transform.position = pos;
-                    box.transform.localScale = new Vector3(stepX, stepY, stepZ);
-                    boxList.Add(box);
+                    // var pos = new Vector3(minPos.x + stepX * i, minPos.y + stepY * j, minPos.z + stepZ * k);
+                    // var box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    // box.transform.position = pos;
+                    // box.transform.localScale = new Vector3(stepX, stepY, stepZ);
+                    // _boxList.Add(box);
+                    var center = new Vector3(min.x + divSize.x * (x + 0.5f), min.y + divSize.y * (y + 0.5f),
+                        min.z + divSize.z * (z + 0.5f));
+                    var newBounds = new Bounds(center, divSize);
+                    _boundsList.Add(newBounds);
                 }
             }
         }
@@ -42,8 +58,14 @@ public class BoundingBox : MonoBehaviour
     private void OnDrawGizmos()
     {
         var mesh = GetComponent<MeshFilter>().sharedMesh;
-        var _bounds = new Bounds(mesh.bounds.center, mesh.bounds.size);
+        var bounds = new Bounds(mesh.bounds.center, mesh.bounds.size);
         Gizmos.color = Color.green;
-        Gizmos.DrawWireCube(_bounds.center, _bounds.size);
+        Gizmos.DrawWireCube(bounds.center, bounds.size);
+        
+        foreach (var newBounds in _boundsList)
+        {
+            Gizmos.color = UnityEngine.Random.ColorHSV();
+            Gizmos.DrawWireCube(newBounds.center, newBounds.size);
+        }
     }
 }
