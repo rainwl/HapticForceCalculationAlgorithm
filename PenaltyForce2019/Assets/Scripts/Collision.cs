f9ad2b714293a71fe4ad277af0bec2ac84dbe3f5
Rain <2215338912@qq.com>
2023-05-16 11:46:17 +0800

    add:collisionenter

---
+++ PenaltyForce2019/Assets/Scripts/Collision.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private void OnCollisionEnter(UnityEngine.Collision other)
+    {
+        Debug.Log("Collision detected between " + gameObject.name + " and " + other.collider.name);
+    }
+}
