cf3763bf1b1000ca34281553b20c88ece083d875
Rain <2215338912@qq.com>
2023-05-16 10:44:20 +0800

    style

--- PenaltyForce2019/Assets/Scripts/DrawSphere.cs
+++ PenaltyForce2019/Assets/Scripts/DrawSphere.cs
@@ -3,18 +3,18 @@ using UnityEngine.Serialization;
 
 public class DrawSphere : MonoBehaviour
 {
-    private Color _drawColor;
+    private Color drawColor;
     public float radius = 0.25f;
+
     private void OnValidate()
     {
         //_drawColor = UnityEngine.Random.ColorHSV();
-        _drawColor = new Color(0, 1, 0, 0.1f);
-
+        drawColor = new Color(0, 1, 0, 0.1f);
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = _drawColor;
+        Gizmos.color = drawColor;
         Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
