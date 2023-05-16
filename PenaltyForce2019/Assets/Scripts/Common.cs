3f80f772f278b64e85077a362c8bd31af697bb3d
Rain <2215338912@qq.com>
2023-05-16 10:32:45 +0800

    feature:common calculate function and penalty force

---
+++ PenaltyForce2019/Assets/Scripts/Common.cs
@@ -0,0 +1,50 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+public class Common
+{
+    /// <summary>
+    /// Calculate the penalty force between the two spheres
+    /// </summary>
+    /// <param name="sphere1"></param>
+    /// <param name="sphere2"></param>
+    /// <returns>penalty force</returns>
+    public static Vector3 PenaltyForce(SphereCollider sphere1, SphereCollider sphere2)
+    {
+        var r1 = sphere1.radius;
+        var r2 = sphere2.radius;
+        var sphere1Center = sphere1.transform.TransformPoint(sphere1.center);
+        var sphere2Center = sphere2.transform.TransformPoint(sphere2.center);
+        var d = Vector3.Distance(sphere1Center, sphere2Center);
+        var volume = Volume(r1, r2, d);
+        var force = Force(volume, sphere1Center, sphere2Center);
+        return force;
+    }
+
+    /// <summary>
+    /// Individual penalty force
+    /// </summary>
+    /// <param name="volume"></param>
+    /// <param name="center1"></param>
+    /// <param name="center2"></param>
+    /// <returns></returns>
+    private static Vector3 Force(float volume, Vector3 center1, Vector3 center2)
+    {
+        var force = volume * (center1 - center2);
+        return force;
+    }
+
+    /// <summary>
+    /// Total intersection volume for two spheres
+    /// </summary>
+    /// <param name="r1">Radius of sphere1</param>
+    /// <param name="r2">Radius of sphere2</param>
+    /// <param name="d">The distance between the centers of two spheres</param>
+    /// <returns>Intersection Volume</returns>
+    private static float Volume(float r1, float r2, float d)
+    {
+        var volume = (math.PI * (r1 + r2 - d) * (r1 + r2 - d) *
+                      (d * d + 2 * d * r2 - 3 * r2 * r2 + 2 * d * r1 + 6 * r1 * r2 - 3 * r1 * r1)) / (12 * d);
+        return volume;
+    }
+}
---
+++ PenaltyForce2019/Assets/Scripts/PenaltyForce.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+
+public class PenaltyForce : MonoBehaviour
+{
+    [SerializeField] private Transform staticObj;
+    [SerializeField] private Transform dynamicObj;
+
+    public Vector3 penaltyForce = Vector3.zero;
+    public Vector3 lastForce = Vector3.zero;
+    private float _penaltyForce;
+    private float _lastForce;
+
+    private readonly List<SphereCollider> _staticList = new List<SphereCollider>();
+    private readonly List<SphereCollider> _dynamicList = new List<SphereCollider>();
+
+    /// <summary>
+    /// Initializing the List<SphereCollider />
+    /// </summary>
+    private void Start()
+    {
+        foreach (Transform child in staticObj)
+        {
+            var sc = child.GetComponent<SphereCollider>();
+            if (sc != null)
+            {
+                _staticList.Add(sc);
+            }
+        }
+
+        foreach (Transform child in dynamicObj)
+        {
+            var sc = child.GetComponent<SphereCollider>();
+            if (sc != null)
+            {
+                _dynamicList.Add(sc);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        var colliding = false;
+        foreach (var sphere1 in _dynamicList)
+        {
+            foreach (var sphere2 in _staticList)
+            {
+                if (sphere1.bounds.Intersects(sphere2.bounds))
+                {
+                    colliding = true;
+                    var force = Common.PenaltyForce(sphere1, sphere2);
+                    penaltyForce += force;
+                    lastForce = penaltyForce;
+                }
+            }
+        }
+
+        if (!colliding)
+        {
+            penaltyForce = Vector3.zero;
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin.label.fontSize = 50;
+        GUI.Label(new Rect(900, 800, 1000, 500), "penalty force: " + penaltyForce);
+        GUI.Label(new Rect(900, 900, 1000, 500), "last force: " + lastForce);
+    }
+}
