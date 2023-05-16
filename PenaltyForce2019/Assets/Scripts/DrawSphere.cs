824c27a1c0aab2790190c6750d50e9c94de6ad3f
Rain <2215338912@qq.com>
2023-05-16 10:43:42 +0800

    feature:draw sphere

---
+++ PenaltyForce2019/Assets/Scripts/DrawSphere.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+public class DrawSphere : MonoBehaviour
+{
+    private Color _drawColor;
+    public float radius = 0.25f;
+    private void OnValidate()
+    {
+        //_drawColor = UnityEngine.Random.ColorHSV();
+        _drawColor = new Color(0, 1, 0, 0.1f);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = _drawColor;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
