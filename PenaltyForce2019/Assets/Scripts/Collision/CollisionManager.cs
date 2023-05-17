05e76c96686c34af24323dc8340e9316366a34ff
Rain <2215338912@qq.com>
2023-05-17 11:15:23 +0800

    format

---
+++ PenaltyForce2019/Assets/Scripts/Collision/CollisionManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    private SingleCollision[] childColliders;
+    private float finalDistance;
+    public Color textColor;
+
+
+    private void Start()
+    {
+        childColliders = GetComponentsInChildren<SingleCollision>();
+    }
+
+    /// <summary>
+    /// we need to change this iterate
+    /// dont iterate each child in childColliders
+    /// just iterate the child who participate in collision
+    /// and we should use dictionary or other high-performance grammar
+    /// </summary>
+    private void Update()
+    {
+        finalDistance = 0f;
+        foreach (var child in childColliders)
+        {
+            finalDistance += child.TotalDistance;
+        }
+    }
+    /// <summary>
+    /// use if to judge Tag,is a loss-performance way ,modify later
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Box"))
+        {
+            foreach (var child in childColliders)
+            {
+                child.IsFirstEnter = false;
+                //finalDistance = 0;
+            }
+            
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.color = textColor;
+        GUI.skin.label.fontSize = 50;
+        GUI.Label(new Rect(900, 800, 1000, 500), "penalty force: " + finalDistance);
+        //GUI.Label(new Rect(900, 900, 1000, 500), "last force: " + lastDistance);
+    }
+}
---
+++ PenaltyForce2019/Assets/Scripts/Collision/SingleCollision.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class SingleCollision : MonoBehaviour
+{
+    // Record the position at the time of the first collision
+    private Vector3 prevPos;
+    // magnitude of Vector3
+    public float TotalDistance { get; set; }
+    public bool IsFirstEnter { get; set; } = false;
+    
+    
+    /// <summary>
+    /// Only record the first collision position
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (IsFirstEnter != false || other.gameObject.CompareTag("Box")) return;
+        IsFirstEnter = true;
+        prevPos = transform.position;
+    }
+    
+    /// <summary>
+    /// I choose distance "a" for calculate and use ”=“ instead of ”+=“
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Box")) return;
+        var currPos = transform.position;
+        var delta = currPos - prevPos;
+        TotalDistance = delta.magnitude;
+    }
+
+    /// <summary>
+    /// When each sphere collider exit the "Box",its distance turns 0
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Box"))
+        {
+            TotalDistance = 0;
+        }
+    }
+    
+}
